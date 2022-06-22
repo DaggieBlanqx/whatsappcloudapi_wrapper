596b53dd1badffe10f3a12984a6acc72180dfaac
daggieblanqx <daggieblanqx@gmail.com>
2022-06-22 13:16:20 +0300

    Updated Code

--- msg_parser.js
+++ msg_parser.js
@@ -1,74 +1,74 @@
 'use strict';
 
 module.exports = ({ requestBody, currentWABA_ID }) => {
-    let input = requestBody;
-
     // THIS FUNCTION IS NOT YET OPTIMIZED FOR PERFORMANCE. IT IS ONLY MADE AS A TEMPORARY SOLUTION.
-    if (!input) {
-        throw new Error('requestBody is required');
+    if (!requestBody) {
+        throw new Error('"requestBody" is required');
     }
 
     if (!currentWABA_ID) {
-        // throw new Error(
-        //     'currentWABA_ID is required. This is the business ID that you have configured in your WABA account.'
-        // );
+        throw new Error(
+            'currentWABA_ID is required. This is the business ID that you have configured in your WABA account.'
+        );
+    }
 
-        // Warn instead of throw error.
+    let WABA_ID = requestBody.entry[0]?.id; // extract the business ID from the webhook payload
+    if (WABA_ID == 0) {
         signale.warn({
-            message:
-                'currentWABA_ID is required. This is the business ID that you have configured in your WABA account.',
+            message: `WABA_ID is 0. You seem to be testing with Meta test subscription. This is not really a valid WABA_ID. I recommend you to send an actual message from an actual whatsapp customer's number.`,
         });
     }
 
-    let WABA_ID = input.entry[0]?.id; // extract the business ID from the webhook payload
-    if (WABA_ID == 0) {
+    if (!WABA_ID || WABA_ID !== currentWABA_ID) {
         throw new Error(
-            `WABA_ID is 0. You seem to be testing with Meta test subscription. This is not really a valid WABA_ID. I recommend you to send an actual message from an actual whatsapp customer's number.`
+            'WABA_ID is not valid. Hint: the message is not intended for this Whatsapp Business Account.'
         );
     }
+
     //first check if the message is a whatsapp message
-    if (!input.object || input.object !== 'whatsapp_business_account') {
+    if (
+        !requestBody.object ||
+        requestBody.object !== 'whatsapp_business_account'
+    ) {
         throw new Error(
             'requestBody is not a valid whatsapp message. Hint: check the "object" property'
         );
     }
 
-    if (!input.entry || !input.entry?.length) {
+    if (!requestBody.entry || !requestBody.entry?.length) {
         throw new Error(
             'requestBody is not a valid whatsapp message. Hint: check the "entry" property'
         );
     }
 
     if (
-        !input.entry[0].changes?.length ||
-        input.entry[0].changes[0].field !== 'messages'
+        !requestBody.entry[0].changes?.length ||
+        requestBody.entry[0].changes[0].field !== 'messages'
     ) {
         throw new Error(
             'requestBody is not a valid whatsapp message. Hint: check the "changes" property'
         );
     }
 
-    let metadata = input.entry[0].changes[0].value.metadata;
-    let contacts = input.entry[0].changes[0].value.contacts?.length
-        ? input.entry[0].changes[0].value.contacts[0]
+    let metadata = requestBody.entry[0].changes[0].value.metadata;
+    let contacts = requestBody.entry[0].changes[0].value.contacts?.length
+        ? requestBody.entry[0].changes[0].value.contacts[0]
         : null;
-    let message = input.entry[0].changes[0].value?.messages?.length
-        ? input.entry[0].changes[0].value.messages[0]
+    let message = requestBody.entry[0].changes[0].value?.messages?.length
+        ? requestBody.entry[0].changes[0].value.messages[0]
         : null;
 
     let actualType;
     if (message?.type) {
         actualType = message.type;
-    } else {
-        if (message?.location) {
-            actualType = 'location';
-        } else if (message?.contacts) {
-            actualType = 'contact';
-        }
+    } else if (message?.location) {
+        actualType = 'location';
+    } else if (message?.contacts) {
+        actualType = 'contact';
     }
 
     let isNotificationMessage;
-    if (input.entry[0].changes[0].value.statuses?.length) {
+    if (requestBody.entry[0].changes[0].value.statuses?.length) {
         isNotificationMessage = true;
     } else {
         if (actualType === 'unsupported' && message.errors?.length) {
@@ -109,13 +109,7 @@ module.exports = ({ requestBody, currentWABA_ID }) => {
         msgType = 'unknownMessage';
     }
 
-    if (!WABA_ID || WABA_ID !== currentWABA_ID) {
-        throw new Error(
-            'WABA_ID is not valid. Hint: the message is not intended for this Whatsapp Business Account.'
-        );
-    }
-
-    let output = {
+    return {
         WABA_ID,
         isNotificationMessage,
         actualType,
@@ -124,6 +118,4 @@ module.exports = ({ requestBody, currentWABA_ID }) => {
         contacts,
         message,
     };
-
-    return output;
 };
